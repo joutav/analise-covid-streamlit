fbd990bd810cb26df2cd1a780949c3da4bb56724
joutav <jvo.maga@gmail.com>
2021-01-13 09:20:15 -0300

    add botoes

--- src/app.py
+++ src/app.py
@@ -52,10 +52,16 @@ def main():
     st.title('Minha aplicação')
     st.markdown('Este trabalho tem o intuito de analisar obitos comparando **2019 e 2020**')
 
-    opcao_doenca = st.sidebar.selectbox('Selecione o tipo de doença', tipo_doenca)
-    opcao_estado = st.sidebar.selectbox('Selecione o tipo de doença', tipo_estado)
+    opcao_doenca = st.sidebar.selectbox('Selecione o tipo de doença', tipo_doenca, 7)
+    opcao_estado = st.sidebar.selectbox('Selecione o tipo de doença', tipo_estado, 27)
     figura = grafico_comparativo(obitos_2019, obitos_2020, opcao_doenca, opcao_estado)
     st.pyplot(figura)
+
+    if st.checkbox('Exibir DataFrame 2019'):
+        st.dataframe(obitos_2019)
+    if st.checkbox('Exibir DataFrame 2020'):
+        st.dataframe(obitos_2020)
+
 
 if __name__ == '__main__':
     main()
